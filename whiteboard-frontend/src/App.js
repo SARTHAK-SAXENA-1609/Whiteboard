e9bb03894ad7b8147a15be02fa58d145e9fe7fc9
Sarthak Saxena <sarsaxena1609@gmail.com>
2025-07-22 10:42:56 +0530

    NC

--- whiteboard-frontend/src/App.js
+++ whiteboard-frontend/src/App.js
@@ -1,9 +1,18 @@
-
+import { useEffect , useRef } from 'react';
 
 function App() {
+  const canvasRef = useRef();
+  useEffect(() =>{
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    context.fillStyle = "00ff00"; // Green color
+    context.fillRect(0, 0, 150 , 75);
+  } ,[]);
+
   return (
-    <div >
-      <h1>My first app 2 </h1>
+    <div className='App'>
+      <canvas id="whiteboard" width="800" height="600"   ></canvas>
+      <h1>My first app </h1>
     </div>
   );
 }
